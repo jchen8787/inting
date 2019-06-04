cf4ca2a8fed0d068aba86f5a47ca67ecd033ad6e
Justin Chen <jchen@fetchrobotics.com>
2019-06-03 17:35:58 -0700

    clener longest comon substring dynamic programming

--- longestCommonSubstring/lcsDP.js
+++ longestCommonSubstring/lcsDP.js
@@ -6,33 +6,24 @@ function lcsDP(s1, s2) {
         dp.push([])
     }
 
-    for (let row of dp) {
-        for (let j = 0; j < s2.length + 1; j++) {
-            row.push('')
-        }
-    }
-
-    // if s1 is empty lcs is empty string
-    for (let j = 0; j < s2.length + 1; j++) {
-        dp[0][j] = ''
-    }
-
-    // if s2 is empty lcs is empty string
     for (let i = 0; i < s1.length + 1; i++) {
-        dp[i][0] = ''
-    }
+        for (let j = 0; j < s2.length + 1; j++) {
+            // if one is empty string lcs is empty string
+            if (i === 0 || j === 0) {
+                dp[i][j] = ''
 
-    for (let i = 0; i < s1.length; i++) {
-        for (let j = 0; j < s2.length; j++) {
+                continue
+            }
+
             const maxPrevString =
-                dp[i][j + 1].length > dp[i + 1][j].length
-                    ? dp[i][j + 1]
-                    : dp[i + 1][j]
+                dp[i-1][j].length > dp[i][j-1].length
+                    ? dp[i-1][j]
+                    : dp[i][j-1]
 
-            if (s1[i] === s2[j]) {
-                dp[i + 1][j + 1] = maxPrevString + s2[j]
+            if (s1[i-1] === s2[j-1]) {
+                dp[i][j] = maxPrevString + s2[j-1]
             } else {
-                dp[i + 1][j + 1] = maxPrevString
+                dp[i][j] = maxPrevString
             }
         }
     }
@@ -42,4 +33,4 @@ function lcsDP(s1, s2) {
     return dp[s1.length][s2.length]
 }
 
-console.log(lcsDP('whatsup', 'zyhatsi'))
+console.log(lcsDP('whatsup', 'zywhasi'))
